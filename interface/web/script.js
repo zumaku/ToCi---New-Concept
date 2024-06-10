827b4327189a2a7e25786fdd9a434dbb7f56e295
zumaku <60200121101@uin-alauddin.ac.id>
2024-06-10 13:23:19 +0800

    Memperbarui struktur folder

---
+++ interface/web/script.js
@@ -0,0 +1,13 @@
+// Fungsi close alert
+const closeButton = document.querySelector('#alert-3 button');
+closeButton.addEventListener('click', function() {
+    const alertDiv = document.querySelector('#alert-3');
+    alertDiv.style.display = 'none';
+});
+
+
+function handleButtonClick(fruit) {
+    // var xhr = new XMLHttpRequest();
+    // xhr.open('GET', '/press?fruit=' + fruit, true);
+    // xhr.send();
+}
